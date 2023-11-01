607fb203bc41945cba5abe64918c02767caea922
Tarun Verma <vermatarun4305@gmail.com>
2023-11-02 00:44:04 +0530

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.json());
 
-app.use('/api/v1', auth);
+// app.use('/api/v1', auth);
 
 app.get("/", async(req, res)=>{
     res.send("Imegration")
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connectDatabase = require('./config/database');
 dotenv.config({ path: require('find-config')('.env') })
 
 //connecting to database
-connectDatabase();
+// connectDatabase();
 const server = app.listen(process.env.PORT || "5000", ()=>{
     console.log(`Server running at Port: 5000`)
 })
