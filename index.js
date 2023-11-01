a7a9ced80d0d4d4838d3ae2a6a1b23d520f6469a
Tarun Verma <vermatarun4305@gmail.com>
2023-11-02 00:04:06 +0530

    first commit

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ dotenv.config({ path: require('find-config')('.env') })
 
 //connecting to database
 connectDatabase();
-const server = app.listen("5000", ()=>{
+const server = app.listen(process.env.PORT || "5000", ()=>{
     console.log(`Server running at Port: 5000`)
 })
