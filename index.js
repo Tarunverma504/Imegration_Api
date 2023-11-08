a5590f6227976997213cd65111aaa0cd0432ec65
Tarun Verma <vermatarun4305@gmail.com>
2023-11-08 23:05:45 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const app = require("./app");
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database');
+const mongoose = require('mongoose');
+
 
 
 //setting up config file
 dotenv.config({ path: require('find-config')('.env') })
 
 //connecting to database
-connectDatabase();
-const server = app.listen(process.env.PORT || "5000", ()=>{
-    console.log(`Server running at Port: 5000`)
-})
+// connectDatabase();
+// const server = app.listen(process.env.PORT || "5000", ()=>{
+//     console.log(`Server running at Port: 5000`)
+// })
+
+mongoose.connect(process.env.DATABASE_URI,{
+}).then(()=>{console.log("MongoDB Database Connected")
+    app.listen(process.env.PORT || "5000", ()=>{
+            console.log(`Server running at Port: 5000`)
+        })
+});
