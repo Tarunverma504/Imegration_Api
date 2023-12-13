f4fe9ef9900eacab982ec5067be4ed21d30651b2
Tarun Verma <vermatarun4305@gmail.com>
2023-12-13 22:29:51 +0530

    first commit

--- controller/authController.js
+++ controller/authController.js
@@ -11,27 +11,86 @@ const {
 require('dotenv').config({ path: require('find-config')('.env') });
 
 exports.registerUser = async(req, res)=>{
+    // try{
+    //     const {name, email, password} = req.body;
+    //     if(name.trim().length<1 || email.trim().length<1 || password.trim().length<1){
+    //         res.status(401).send({ message: 'Please fill all the details'});
+    //     }
+    //     const result = await User.find({email:email});
+    //     if(result && result.length==0){
+    //         let user = await User.create({
+    //             name,   
+    //             email,
+    //             password
+    //         })
+    //         if(user!=null){
+    //             console.log(user);
+    //             const msg = `<p>
+    //             Dear Sir/Ma'am,<br/><br/>
+    //             We hope this email finds you hale & hearty.<br/><br/>
+    //             We want to express our sincere gratitude for accepting the invitation to collaborate with The Immigration Gurus. It's an absolute honor to partner with someone as influential and respected as you.<br/><br/>
+    //             Please use the below given Unique Link to share it with your Videos or Posts to get Expression of Interest from your audience - <br/>
+    //             <p><a href=https://dashboard.theimmigrationgurus.co.in/customer-form/${user._id}>Click Here</a> for further steps</p> <br/><br/>
+    //             We are thrilled about the opportunity to work together and leverage your platform to promote Immigration Services for Canada, Australia, UK, USA & NZ, etc. Your audience's trust and engagement are truly invaluable, and we are excited about the potential impact our collaboration can have for our mutual growth.<br/><br/>
+    //             We are fully committed to ensuring this partnership is mutually beneficial. Please feel free to share any specific preferences, guidelines, or ideas you have in mind for our affiliate marketing efforts. Your expertise and insights will be instrumental in crafting compelling and authentic content.<br/><br/>
+    //             We look forward to a successful and fruitful partnership ahead. If there's anything else you need or any additional information we can provide, please don't hesitate to reach out.<br/><br/>
+    //             Once again, thank you for this fantastic opportunity. We are excited about the journey ahead and eager to create valuable content together.<br/>
+    //             <br/><br/>
+    //             Warm regards,<br/>
+    //             Marketing Team, The Immigration Gurus<br/>
+    //             Email: <a href = "mailto:support@immigurus.com">support@immigurus.com</a><br/>
+    //             Mob: <a href="tel:+91-9812380882">+91-9812380882</a>
+                
+    //             `
+    //             if(sendMail(email, msg, "Immigurus Invitation"))
+    //             {
+    //                 console.log("Mail send successfully")
+    //                 res.status(200).json({name:user.name, email:user.email, role: user.role, id:user._id})
+    //             }
+    //             else
+    //             {
+    //                 res.status(504).send({ message: 'Internalserver error'});
+    //             }
+    //         }
+    //         else{
+    //             res.status(500).send({ message: 'Internalserver error'});
+    //         }
+    //     }
+    //     else{
+    //         res.status(401).send({ message: 'Email is Already Registered'});
+    //     }
+    // }
+    // catch(err){
+    //     if(err && err.name && err.name === 'ValidationError'){
+    //         const message = Object.values(err.errors).map(val => val.message)
+    //         res.status(403).send({ message: message[0]});
+    //     }
+    //     else{
+    //         console.log(err);
+    //         res.status(500).json({ message: 'Internalserver error'});
+    //     }
+    // }
+
+
     try{
-        
         const {name, email, password} = req.body;
         if(name.trim().length<1 || email.trim().length<1 || password.trim().length<1){
             res.status(401).send({ message: 'Please fill all the details'});
         }
         const result = await User.find({email:email});
         if(result && result.length==0){
-            let user = await User.create({
+            await User.create({
                 name,   
                 email,
                 password
             })
-            if(user!=null){
-                console.log(user);
+            .then((data)=>{
                 const msg = `<p>
                 Dear Sir/Ma'am,<br/><br/>
                 We hope this email finds you hale & hearty.<br/><br/>
                 We want to express our sincere gratitude for accepting the invitation to collaborate with The Immigration Gurus. It's an absolute honor to partner with someone as influential and respected as you.<br/><br/>
                 Please use the below given Unique Link to share it with your Videos or Posts to get Expression of Interest from your audience - <br/>
-                <p><a href=https://dashboard.theimmigrationgurus.co.in/customer-form/${user._id}>Click Here</a> for further steps</p> <br/><br/>
+                <p><a href=https://dashboard.theimmigrationgurus.co.in/customer-form/${data._id}>Click Here</a> for further steps</p> <br/><br/>
                 We are thrilled about the opportunity to work together and leverage your platform to promote Immigration Services for Canada, Australia, UK, USA & NZ, etc. Your audience's trust and engagement are truly invaluable, and we are excited about the potential impact our collaboration can have for our mutual growth.<br/><br/>
                 We are fully committed to ensuring this partnership is mutually beneficial. Please feel free to share any specific preferences, guidelines, or ideas you have in mind for our affiliate marketing efforts. Your expertise and insights will be instrumental in crafting compelling and authentic content.<br/><br/>
                 We look forward to a successful and fruitful partnership ahead. If there's anything else you need or any additional information we can provide, please don't hesitate to reach out.<br/><br/>
@@ -46,16 +105,21 @@ exports.registerUser = async(req, res)=>{
                 if(sendMail(email, msg, "Immigurus Invitation"))
                 {
                     console.log("Mail send successfully")
-                    res.status(200).json({name:user.name, email:user.email, role: user.role, id:user._id})
+                    res.status(200).json({name:data.name, email:data.email, role: data.role, id:data._id})
                 }
                 else
                 {
                     res.status(504).send({ message: 'Internalserver error'});
                 }
-            }
-            else{
-                res.status(500).send({ message: 'Internalserver error'});
-            }
+            })
+            // if(user!=null){
+            //     console.log(user);
+                
+                
+            // }
+            // else{
+            //     res.status(500).send({ message: 'Internalserver error'});
+            // }
         }
         else{
             res.status(401).send({ message: 'Email is Already Registered'});
